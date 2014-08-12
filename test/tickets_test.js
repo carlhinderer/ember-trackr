858ba993b09230b27eed6dd6be55732bd1b5867a
Carl Hinderer <carl.hinderer4@gmail.com>
2014-08-12 15:26:37 -0400

    Added ticket details template.

--- test/tickets_test.js
+++ test/tickets_test.js
@@ -12,3 +12,13 @@ test('listing tickets', function() {
         'expected to find Ticket 1');
     });
 });
+
+test('viewing ticket details', function() {
+  visit('/').
+    click('a:contains("Tickets")').
+    click('a:contains("Ticket 1")').
+    then(function() {
+      ok(find('*:contains("A description for the ticket here.")').length,
+	'expected to find ticket description');
+    });
+});
