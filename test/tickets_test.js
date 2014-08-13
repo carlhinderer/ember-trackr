d4ae7040aa31627cae2ac240c60467b86d7f10dd
Carl Hinderer <carl.hinderer4@gmail.com>
2014-08-12 23:00:10 -0400

    Added ability to edit ticket status and description.

--- test/tickets_test.js
+++ test/tickets_test.js
@@ -27,6 +27,8 @@ test('editing ticket details', function() {
   visit('/tickets/1').
     click('button:contains("Edit")').
     fillIn('input[name="title"]', 'Foo Bar').
+    fillIn('select[name="status"]', 'Open').
+    fillIn('textarea[name="description"]', 'New description').
     click('button:contains("Done")').
     then(function() {
       ok(find('.list-group-item:contains("Foo Bar")').length,
@@ -34,5 +36,14 @@ test('editing ticket details', function() {
 
       ok(find('.panel-title:contains("Foo Bar")').length,
 	'expected title in detail view to update');
+
+      ok(find('.list-group-item .badge:contains("Open")').length,
+	'expected status in master list to update');
+      
+      ok(find('.panel-title .badge:contains("Open")').length,
+	'expected status in detail view to update');
+
+      ok(find('.panel-body:contains("New description")').length,
+	'expected description to update');
     });
 });
